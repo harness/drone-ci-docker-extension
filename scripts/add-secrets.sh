1e9652c37417617a761c3a20d5fe2498ee005b24
Kamesh <kamesh.sampath@hotmail.com>
2022-08-31 22:48:47 +0530

    ci: auto tag dev builds

--- scripts/add-secrets.sh
+++ scripts/add-secrets.sh
@@ -4,6 +4,6 @@ set -euo pipefail
 
 drone secret add --name destination_image --data "docker.io/drone/drone-desktop-docker-extension" "harness/drone-desktop-docker-extension"
 
-drone secret add --name extn_github_token --data "$HARNESS_CI_PAT" "harness/drone-desktop-docker-extension"
+drone secret add --name extn_github_token --data "$DRONEIO_CI_PAT" "harness/drone-desktop-docker-extension"
 
 drone secret add --name image_registry --data "docker.io" "harness/drone-desktop-docker-extension"
